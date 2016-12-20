7c9d9e6d2ccce6236ab490cc02b2bdc3124fba9a
radames <radames@infoway-pi.com.br>
2016-12-20 11:27:29 -0300

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fxImoveisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fxImoveisApp
+ */
+angular.module('fxImoveisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
